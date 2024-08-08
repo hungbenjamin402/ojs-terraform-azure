723112917f99bb67ee188dd2f2dfb8190d9bc6d8
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-08-08 01:21:10 -0700

    added variables for environment, resource group name, location

---
+++ terraform-manifests/c2-generic-input-variables.tf
@@ -0,0 +1,21 @@
+# Environment variable
+variable "environment" {
+  description = "Environment Name used as prefix"
+  type        = string
+  default = "dev"
+}
+
+# Resource Group name in azure
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type = string
+  default = "rg-ojs"
+}
+
+# Azure Resource Location
+variable "resource_group_location" {
+  description = "Region to create azure resources"
+  type = string
+  default = "West US" 
+}
+
