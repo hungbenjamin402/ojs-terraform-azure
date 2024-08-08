4de7802ab6cc1aeffae2278dc55951e7d26f2bf1
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-08-08 01:22:14 -0700

    updated name and used locals + vars

--- terraform-manifests/c2-resource-group.tf
+++ terraform-manifests/c2-resource-group.tf
@@ -1,5 +1,11 @@
 # Resource-1: Azure Resource
+# resource "azurerm_resource_group" "ojs_rg" {
+#   name     = "ojs-resources"
+#   location = "West US"
+# }
+
 resource "azurerm_resource_group" "ojs_rg" {
-  name     = "ojs-resources"
-  location = "West US"
+  name     = "${local.environment}-${var.resource_group_name}-${random_string.myrandom.id}"
+  location = var.resource_group_location
+  tags = local.common_tags
 }
