30ea64e4585e80852ae3e8310a6b20b5c3d9505c
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-08-10 00:10:33 -0700

    added null provisioner and updated azurerm provider

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -1,12 +1,30 @@
+#Terraform block
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = "3.113.0"
+#     }
+#   }
+# }
 terraform {
+  required_version = ">= 1.0.0" #terraform cli version
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.113.0"
+      version = ">= 2.0" #azurerm provider version
     }
+    random = { #random provider info
+      source = "hashicorp/random" 
+      version = ">= 3.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = ">= 3.0"
+    }     
   }
 }
-
+#Provider block
 provider "azurerm" {
   # Configuration options
   features {}
