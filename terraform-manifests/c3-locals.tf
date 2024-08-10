391e612a02a64003b722df9652a8ebbe478774e7
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-08-10 00:09:50 -0700

    added bastion local

--- terraform-manifests/c3-locals.tf
+++ terraform-manifests/c3-locals.tf
@@ -1,7 +1,7 @@
 locals {
     environment = var.environment
     #resource_name_prefix = "${var.environment}"
-
+    bastion_host = "${var.environment}-${var.bastion_vm}"
     common_tags = {
         environment = local.environment
     }
