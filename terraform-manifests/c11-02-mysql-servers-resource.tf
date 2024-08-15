07fd7362f73cd9bc7e5937d6b47aeca14d3dcaa6
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-08-14 23:50:54 -0700

    changed unsused firewall rule and turned off ssl to connect

--- terraform-manifests/c11-02-mysql-servers-resource.tf
+++ terraform-manifests/c11-02-mysql-servers-resource.tf
@@ -9,6 +9,7 @@ resource "azurerm_mysql_flexible_server" "ojs_server" {
   sku_name               = "B_Standard_B4ms"
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
+  delegated_subnet_id = module.vnet.vnet_subnets[2]
 }
 
 # Resource-2: Azure MySQL Database / Schema
@@ -26,5 +27,12 @@ resource "azurerm_mysql_flexible_server_firewall_rule" "ojs_fw_rule" {
   resource_group_name = azurerm_resource_group.ojs_rg.name
   server_name         = azurerm_mysql_flexible_server.ojs_server.name
   start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
+
+resource "azurerm_mysql_flexible_server_configuration" "ojs_server_ssl_config" {
+  resource_group_name = azurerm_resource_group.ojs_rg.name
+  server_name = azurerm_mysql_flexible_server.ojs_server.name
+  name      = "require_secure_transport"
+  value     = "off"
 }
