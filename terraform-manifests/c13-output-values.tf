aee8d54b2ed40892af10d090024f7b68407f1a18
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-08-15 06:58:32 -0700

    updated outputs

--- terraform-manifests/c13-output-values.tf
+++ terraform-manifests/c13-output-values.tf
@@ -5,9 +5,9 @@
 # }
 
 # Output the public IP address
-output "ojs_public_ip" {
-  value = azurerm_public_ip.ojs_public_ip.ip_address
-}
+# output "ojs_public_ip" {
+#   value = azurerm_public_ip.ojs_public_ip.ip_address
+# }
 
 # # Output the MySQL server FQDN
 # output "ojs_db_fqdn" {
@@ -15,6 +15,6 @@ output "ojs_public_ip" {
 # }
 
 # Output the MySQL server FQDN
-output "ojs_db_fqdn" {
-  value = azurerm_mysql_flexible_server.ojs_db.fqdn
-}
+# output "ojs_db_fqdn" {
+#   value = azurerm_mysql_flexible_server.ojs_db.fqdn
+# }
