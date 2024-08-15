19febf240136d959600297dd939c5ec9946d6aa3
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-08-15 00:08:20 -0700

    service endpoint and subnet delegation for db, nsgs for web and db
    Co-authored by: Benjamin Hung Nguyen hungbenjamin402@gmail.com
    Co-authored by: Isaac Palompo igapalompo@gmail.com

--- terraform-manifests/c6-02-virtual-network.tf
+++ terraform-manifests/c6-02-virtual-network.tf
@@ -1,19 +1,4 @@
-# # Create a virtual network
-# resource "azurerm_virtual_network" "ojs_vnet" {
-#   name                = "ojs-network"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.ojs_rg.location
-#   resource_group_name = azurerm_resource_group.ojs_rg.name
-# }
-
-# # Create a subnet
-# resource "azurerm_subnet" "ojs_subnet" {
-#   name                 = "ojs-subnet"
-#   resource_group_name  = azurerm_resource_group.ojs_rg.name
-#   virtual_network_name = azurerm_virtual_network.ojs_vnet.name
-#   address_prefixes     = ["10.0.1.0/24"]
-# }
-
+# Vnet module to create vnet, subnets, and more
 module "vnet" {
   source  = "Azure/vnet/azurerm"
   version = "4.1.0"
@@ -27,16 +12,40 @@ module "vnet" {
   subnet_prefixes     = [var.web_subnet_address[0], var.app_subnet_address[0], var.db_subnet_address[0], var.bastion_subnet_address[0]]
   # not sure why but sorted in alphabetical order (stored as app, bastion, db, web)
   subnet_names        = [var.web_subnet_name, var.app_subnet_name, var.db_subnet_name, var.bastion_subnet_name]
+  
   subnet_service_endpoints = {
-    "${var.db_subnet_name}" = ["Microsoft.Storage", "Microsoft.Sql"],
-    #"${var.app_subnet_name}" = ["Microsoft.Storage", "Microsoft.Sql"],
-    "${var.web_subnet_name}" = ["Microsoft.Storage", "Microsoft.Sql"]
+    "${var.db_subnet_name}" = ["Microsoft.Storage", "Microsoft.Sql"] #,
    
+  }
+  subnet_delegation = {
+    
+    "${var.db_subnet_name}"  = {
+      "Microsoft.Sql.managedInstances" = {
+        service_name = "Microsoft.Sql/managedInstances"
+        service_actions = [
+          "Microsoft.Network/virtualNetworks/subnets/join/action",
+          "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+          "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
+        ]
+      } 
+    },  
+    "${var.db_subnet_name}" = {
+      "Microsoft.DBforMySQL/flexibleServers" = {
+        service_name = "Microsoft.DBforMySQL/flexibleServers"
+        service_actions = [
+          "Microsoft.Network/virtualNetworks/subnets/join/action",
+          "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+          "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
+        ]
+      }
+    }
   }
 
   nsg_ids = {
-    "${var.web_subnet_name}" = "${azurerm_network_security_group.app_subnet_nsg.id}"
+    "${var.web_subnet_name}" = "${azurerm_network_security_group.web_subnet_nsg.id}"
     "${var.bastion_subnet_name}" = "${azurerm_network_security_group.bastion_subnet_nsg.id}"
+    "${var.db_subnet_name}" = "${azurerm_network_security_group.db_subnet_nsg.id}"
+
   }
   tags = {
     environment = "${local.environment}"
@@ -53,39 +62,6 @@ module "vnet" {
 #   location            = azurerm_resource_group.ojs_rg.location
 #   resource_group_name = azurerm_resource_group.ojs_rg.name
 #   allocation_method   = "Dynamic"
-# }
-
-# Create a network security group and rule
-# resource "azurerm_network_security_group" "ojs_nsg" {
-#   name                = "ojs-nsg"
-#   location            = azurerm_resource_group.ojs_rg.location
-#   resource_group_name = azurerm_resource_group.ojs_rg.name
-
-#   security_rule {
-#     name                       = "HTTP"
-#     priority                   = 1001
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "80"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-# }
-
-# Create a network interface
-# resource "azurerm_network_interface" "ojs_nic" {
-#   name                = "ojs-nic"
-#   location            = azurerm_resource_group.ojs_rg.location
-#   resource_group_name = azurerm_resource_group.ojs_rg.name
-
-#   ip_configuration {
-#     name                          = "ojs-nic-config"
-#     subnet_id                     = azurerm_subnet.ojs_subnet.id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = azurerm_public_ip.ojs_public_ip.id
-#   }
 # }
 
 resource "azurerm_network_interface" "ojs_nic" {
